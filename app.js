17520484d31ab38d11a1f9d01cdc79042eeb0bca
Maya Buser De <maya.buser@gmail.com>
2021-02-11 15:57:17 -0500

    accordion now opens only one at a time. Cleaned up Css

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 //toggle hidden class on P element in Ul of faqs
 
-const ul = document.querySelector('.faqs');
+const accItem = document.querySelectorAll('.faq');
+const accHeading = document.querySelectorAll('.faq-title');
 
-ul.addEventListener('click', function (e) {
+// add event listener to the heading of each list item
+for (i = 0; i < accHeading.length; i++) {
 
-    if (e.target.nodeName === 'H2') {
-        e.target.classList.toggle('selected');
-        e.target.nextElementSibling.classList.toggle('hidden');
-    }
-})
+    accHeading[i].addEventListener('click', function () {
+        //gets the class names on the parent of the clicked item i.e. the li item
+        let itemClass = this.parentNode.className;
+        //set all the list items to hidden. In css use ".hidden p" to access the para to hide
+        for (j = 0; j < accItem.length; j++) {
+            accItem[j].className = 'faq hidden';
+        }
+        //for the list item that was clicked on change the classes to "faq selected". In addition to removing ".hidden p", in css use ".selected .faq-title" to change the styling of the selected list item
+        if (itemClass == 'faq hidden') {
+            this.parentNode.className = 'faq selected';
+        }
+    })
+
+
+}
